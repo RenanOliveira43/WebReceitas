69998ee09b6e31aac5c61fc041afd9e576828bca
Renan <renanneves430@gmail.com>
2024-08-10 23:49:00 -0300

    Update app

--- api/app.js
+++ api/app.js
@@ -4,11 +4,14 @@ const path = require('path');
 const app = express();
 
 let recipes = [];
+const superSenha = 'vandaobobao';
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
+
+const verificarSenha = (senha) => senha === superSenha;
 
 app.get('/', (req, res) => {
     res.render('index', { recipes });
@@ -19,9 +22,13 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/add', (req, res) => {
-    const { title, description } = req.body;
-    recipes.push({ id: recipes.length + 1, title, description: description.replace(/\n/g, '<br>') });
-    res.redirect('/');
+    const { title, description, password } = req.body;
+    if (verificarSenha(password)) {
+        recipes.push({ id: recipes.length + 1, title, description: description.replace(/\n/g, '<br>') });
+        res.redirect('/');
+    } else {
+        res.status(403).send('Senha incorreta');
+    }
 });
 
 app.get('/edit/:id', (req, res) => {
@@ -30,16 +37,29 @@ app.get('/edit/:id', (req, res) => {
 });
 
 app.post('/edit/:id', (req, res) => {
-    const { title, description } = req.body;
-    const recipe = recipes.find(r => r.id === parseInt(req.params.id));
-    recipe.title = title;
-    recipe.description = description.replace(/\n/g, '<br>');
-    res.redirect('/');
+    const { title, description, password } = req.body;
+    if (verificarSenha(password)) {
+        const recipe = recipes.find(r => r.id === parseInt(req.params.id));
+        if (recipe) {
+            recipe.title = title;
+            recipe.description = description.replace(/\n/g, '<br>');
+            res.redirect('/');
+        } else {
+            res.status(404).send('Receita não encontrada');
+        }
+    } else {
+        res.status(403).send('Senha incorreta');
+    }
 });
 
 app.get('/delete/:id', (req, res) => {
-    recipes = recipes.filter(r => r.id !== parseInt(req.params.id));
-    res.redirect('/');
+    const password = req.query.password;
+    if (verificarSenha(password)) {
+        recipes = recipes.filter(r => r.id !== parseInt(req.params.id));
+        res.redirect('/');
+    } else {
+        res.status(403).send('Senha incorreta');
+    }
 });
 
 module.exports = app;
